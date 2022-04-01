566f099450cddf2c45c6b080b6697e2f697d417a
Khushalvermah <89624505+Khushalvermah@users.noreply.github.com>
2022-04-01 17:56:17 +0530

    Add files via upload
    Webbar2.0

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let burger = document.querySelector('.burger');
+let navbr = document.querySelector('.navigation');
+
+burger.addEventListener('click', () => {
+
+    navbr.classList.toggle('navresp');
+
+
+});
+
+
+let email = document.getElementById('email').value;
+let  body = document.getElementsByClassName('mainpost').value;
+let username = document.getElementsByClassName(mail).value;
+
